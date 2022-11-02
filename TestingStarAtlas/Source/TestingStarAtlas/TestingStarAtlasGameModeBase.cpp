a1f25c89cf5664c90e215accd964f9f747ddce2b
PAOK_2001 <2001.paok@gmail.com>
2022-11-02 11:11:39 -0600

    Tested account subcription

--- TestingStarAtlas/Source/TestingStarAtlas/TestingStarAtlasGameModeBase.cpp
+++ TestingStarAtlas/Source/TestingStarAtlas/TestingStarAtlasGameModeBase.cpp
@@ -6,9 +6,14 @@
 #include "WalletAccount.h"
 #include "SolanaUtils/Wallet.h"
 #include "Network/UFRequestManager_WB.h"
+#include "Network/SubscriptionUtils.h"
 void ATestingStarAtlasGameModeBase::testWebSocket(const FString& pubKey)
 {
 	UWalletAccount* demoWallet = NewObject<UWalletAccount>();
 	UFRequestManager_WB* SocketManager = Cast<UFRequestManager_WB>(GetGameInstance());
-	demoWallet->GetAcountBalance_WB(pubKey, SocketManager);
+	demoWallet->Sub2AccountInfo(pubKey, SocketManager);
+	GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Green, "Subcription Confirmed");
+	//double result = FSubscriptionUtils::GetAccountSubInfo();
+	//GEngine->AddOnScreenDebugMessage(-1, 15.0f, FColor::Yellow, FString::SanitizeFloat(result));
+
 }
